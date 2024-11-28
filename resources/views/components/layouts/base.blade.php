a3708caaf9814e402d56c6851798685592466cfb
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-11-28 13:57:52 +0100

    Add favicon

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -5,6 +5,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
         <title>{{ $title ?? 'Page Title' }}</title>
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
