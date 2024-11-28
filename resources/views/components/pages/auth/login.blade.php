82bfabde349fae51511bd5eafce2c5579ea1fee4
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-11-28 14:58:25 +0100

    Update TACSS to use new input styles

--- resources/views/components/pages/auth/login.blade.php
+++ resources/views/components/pages/auth/login.blade.php
@@ -1,21 +1,28 @@
-<form wire:submit="login">
-    <span>
-        <span>
-            <label for="email">Email</label>
-            <input type="email" id="email" wire:model="email" autocomplete="username" required>
-        </span>
-        <span>
-            <label for="password">Wachtwoord</label>
-            <input type="password" id="password" wire:model="password" autocomplete="current-password" required>
-        </span>
-        <span>
-            <label for="remember">Ingelogd blijven</label>
-            <input type="checkbox" id="remember" wire:model="remember">
-        </span>
-        <input type="submit" value="Ok" class="btn solid swap active">
-        @if (session()->has('error'))
-            <h1>{{ session('error') }}</h1>
-        @endif
-        @csrf
-    </span>
-</form>
+<x-slot:header>
+    <x-parts.brand />
+</x-slot:header>
+<x-slot:footer>
+    <x-parts.copyright />
+    <x-parts.contact />
+</x-slot:footer>
+<div class="fill flex center">
+    <div class="card content shadow secondary apply">
+        <form class="flex center gap" wire:submit="login">
+            <h3>Log in</h3>
+            <span class="flex">
+                <label for="email">Email</label>
+                <input type="email" id="email" wire:model="email" autocomplete="username" required>
+            </span>
+            <span class="flex">
+                <label for="password">Wachtwoord</label>
+                <input type="password" id="password" wire:model="password" autocomplete="current-password" required>
+            </span>
+            <span>
+                <label for="remember">Ingelogd blijven</label>
+                <input type="checkbox" id="remember" wire:model="remember">
+            </span>
+            <input type="submit" value="Ok" class="btn solid swap fill primary">
+            @csrf
+        </form>
+    </div>
+</div>
