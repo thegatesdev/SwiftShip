7d3408232726eb4338dce81e63ece5e1493c4e10
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-11-29 11:41:43 +0100

    Allow null user in packet type migration

--- database/migrations/0001_01_01_000003_create_packet_types_table.php
+++ database/migrations/0001_01_01_000003_create_packet_types_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('packet_types', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->nullable()->constrained();
             $table->string('name');
             $table->string('description');
 
