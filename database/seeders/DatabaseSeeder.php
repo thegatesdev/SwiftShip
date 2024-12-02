9f424cd9ec3027eccdbe3b92f83943cc74f6be31
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-12-02 10:43:30 +0100

    Create users per role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Authorization\RolesEnum;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +11,21 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(PermissionSeeder::class);
+
         User::factory()->create([
             'email' => 'admin@example.com',
-        ]);
+        ])->assignRole(RolesEnum::ADMIN);
+
+        User::factory()->create([
+            'email' => 'delivery@example.com',
+        ])->assignRole(RolesEnum::DELIVERY);
+
+        User::factory()->create([
+            'email' => 'sender@example.com',
+        ])->assignRole(RolesEnum::SENDER);
+
+        User::factory()->create([
+            'email' => 'warehouse@example.com',
+        ])->assignRole(RolesEnum::WAREHOUSE);
     }
 }
