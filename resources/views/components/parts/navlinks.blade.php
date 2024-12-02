5e85a87225e936ccdf59377928ae86c1d62e6ee1
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-12-02 10:48:22 +0100

    Apply permissions in visible navlinks

--- resources/views/components/parts/navlinks.blade.php
+++ resources/views/components/parts/navlinks.blade.php
@@ -1,4 +1,8 @@
 <ul class="list col hover">
+    @can('packet_type_create')
     <li><a href="{{ route('app.pt.create') }}" wire:navigate>Nieuw Pakket Type</a></li>
+    @endcan
+    @can('packet_create')
     <li><a href="{{ route('app.packet.create') }}" wire:navigate>Nieuw Pakket</a></li>
+    @endcan
 </ul>
