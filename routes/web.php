a1f00a4ec192ff3b50383e28db64f0d0175af057
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-12-02 10:46:24 +0100

    Fix route permission mistake

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,13 @@ Route::middleware(['auth'])->prefix('app')->name('app.')->group(function () {
     Route::get('/', AppDashboard::class)->name('dashboard');
     Route::get('/confirm', PasswordConfirm::class)->name('password.confirm');
 
-    Route::get('/type/new', PTCreate::class)->name('pt.create');
-    Route::get('/type/{packetType}', PTUpdate::class)->name('pt.update');
+    Route::get('/type/new', PTCreate::class)->name('pt.create')
+        ->middleware('can:packet_type_create');
+    Route::get('/type/{packetType}', PTUpdate::class)->name('pt.update')
+        ->middleware('can:packet_type_create');
 
     Route::get('/packet/new', PacketCreate::class)->name('packet.create')
-        ->middleware('can:packet_type_create');
+        ->middleware('can:packet_create');
     Route::get('/packet/{packet}', PacketUpdate::class)->name('packet.update')
         ->middleware('can:packet_create');
 });
