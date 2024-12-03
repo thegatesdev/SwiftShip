664631dc75acf9075397f49e2158cf106dc12f27
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-12-03 12:07:54 +0100

    Account for empty values

--- resources/views/components/pages/app/traffic-api.blade.php
+++ resources/views/components/pages/app/traffic-api.blade.php
@@ -12,21 +12,21 @@
 <div class="fill flex scroll pad secondary">
     @foreach ($data->roads as $road)
         <x-parts.collapse name="{{ $road->road }}">
-            @foreach ($road->segments as $segment)
-                <x-parts.collapse name="{{ $segment->start . ' - ' . $segment->end }}">
+            @foreach ($road->segments ?? [] as $segment)
+                <x-parts.collapse name="{{ ($segment->start ?? 'Onbekend') . ' - ' . ($segment->end ?? 'Onbekend') }}">
                     @foreach ($segment->jams ?? [] as $jam)
-                        <x-parts.collapse name="File: {{ $jam->from . ' - ' . $jam->to }}">
-                            {{ $jam->reason }}
+                        <x-parts.collapse name="File: {{ ($jam->from ?? 'Onbekend') . ' - ' . ($jam->to ?? 'Onbekend') }}">
+                            {{ $jam->reason ?? 'Onbekend' }}
                         </x-parts.collapse>
                     @endforeach
                     @foreach ($segment->roadworks ?? [] as $roadwork)
-                        <x-parts.collapse name="Wegwerkzaamheden: {{ $roadwork->incidentType }}">
-                            {{ $roadwork->reason }}
+                        <x-parts.collapse name="Wegwerkzaamheden: {{ ($roadwork->incidentType ?? 'Onbekend incident') }}">
+                            {{ $roadwork->reason ?? 'Onbekend' }}
                         </x-parts.collapse>
                     @endforeach
                     @foreach ($segment->radars ?? [] as $radar)
-                        <x-parts.collapse name="Radar: {{ $radar->from . ' - ' . $radar->to }}">
-                            {{ $radar->reason }}
+                        <x-parts.collapse name="Radar: {{ ($radar->from ?? 'Onbekend') . ' - ' . ($radar->to ?? 'Onbekend') }}">
+                            {{ $radar->reason ?? 'Onbekend' }}
                         </x-parts.collapse>
                     @endforeach
                 </x-parts.collapse>
